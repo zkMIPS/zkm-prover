fc0b496a2f1499c51627bd8f870c65d256542886
xander42280 <xander.z@zkm.io>
2024-03-16 14:30:21 +0800

    Add debugging information

--- service/src/prover_client.rs
+++ service/src/prover_client.rs
@@ -37,6 +37,7 @@ pub async fn get_idle_client(
     for node in nodes {
         let client = is_active(&node.addr, tls_config.clone()).await;
         if let Some(client) = client {
+            println!("get_idle_client success");
             return Some(client);
         }
     }
