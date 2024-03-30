4ee35f0c839958d885fc769a80076188a5dda23b
evan95s <164893154+evan95s@users.noreply.github.com>
2024-03-30 20:29:29 +0800

    feat: add tls to endpoints (#11)

---
+++ service/config/README.md
@@ -0,0 +1,19 @@
+# README
+
+## Description
+
+The script file `gen_config.sh` allow you generate multi prover toml in a easy way.
+
+First, you should set these variables according to your environment.
+
+- provers
+- stage
+- snarks
+- tls
+- base_dir
+
+Then you can run this script in below way.
+
+```bash
+bash gen_config.sh
+```
--- service/examples/stage.rs
+++ service/examples/stage.rs
@@ -1,11 +1,12 @@
+use common::tls::Config;
 use stage_service::stage_service_client::StageServiceClient;
 use stage_service::{BlockFileItem, GenerateProofRequest};
-
 use std::env;
 use std::fs;
 use std::path::Path;
-
 use std::time::Instant;
+use tonic::transport::ClientTlsConfig;
+use tonic::transport::Endpoint;
 
 pub mod stage_service {
     tonic::include_proto!("stage.v1");
@@ -17,8 +18,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let block_path = env::var("BLOCK_PATH").unwrap_or("/tmp/zkm/test/0_13284491".to_string());
     let block_no = env::var("BLOCK_NO").unwrap_or("13284491".to_string());
     let block_no = block_no.parse::<_>().unwrap_or(13284491);
-    let seg_size = env::var("SEG_SIZE").unwrap_or("262144".to_string());
-    let seg_size = seg_size.parse::<_>().unwrap_or(262144);
+    let seg_size = env::var("SEG_SIZE").unwrap_or("16384".to_string());
+    let seg_size = seg_size.parse::<_>().unwrap_or(16384);
+    let endpoint = env::var("ENDPOINT").unwrap_or("http://127.0.0.1:50000".to_string());
+    let ca_cert_path = env::var("CA_CERT_PATH").unwrap_or("".to_string());
+    let cert_path = env::var("CERT_PATH").unwrap_or("".to_string());
+    let key_path = env::var("KEY_PATH").unwrap_or("".to_string());
+    let ssl_config = if ca_cert_path.is_empty() {
+        None
+    } else {
+        Some(Config::new(ca_cert_path, cert_path, key_path).await?)
+    };
 
     let elf_data = prover::provers::read_file_bin(&elf_path).unwrap();
     let mut block_data = Vec::new();
@@ -47,7 +57,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
     println!("request: {:?}", request.proof_id.clone());
     let start = Instant::now();
-    let mut stage_client = StageServiceClient::connect("http://127.0.0.1:50000").await?;
+    let endpoint = match ssl_config {
+        Some(config) => {
+            let tls_config = ClientTlsConfig::new()
+                .ca_certificate(config.ca_cert)
+                .identity(config.identity);
+            Endpoint::new(endpoint)?.tls_config(tls_config)?
+        }
+        None => Endpoint::new(endpoint)?,
+    };
+    let mut stage_client = StageServiceClient::connect(endpoint).await?;
     let response = stage_client.generate_proof(request).await?.into_inner();
     println!("response: {:?}", response);
     let end = Instant::now();
