42aed181745c31789f45bc4984c46717dbc1e900
xander42280 <xander.z@zkm.io>
2024-09-08 12:57:35 +0800

    cargo fmt

--- service/build.rs
+++ service/build.rs
@@ -1,5 +1,15 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().protoc_arg("--experimental_allow_proto3_optional").compile(&["proto/src/proto/prover/v1/prover.proto"], &["proto/src/proto"])?;
-    tonic_build::configure().protoc_arg("--experimental_allow_proto3_optional").compile(&["proto/src/proto/stage/v1/stage.proto"], &["proto/src/proto"])?;
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(
+            &["proto/src/proto/prover/v1/prover.proto"],
+            &["proto/src/proto"],
+        )?;
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(
+            &["proto/src/proto/stage/v1/stage.proto"],
+            &["proto/src/proto"],
+        )?;
     Ok(())
 }
