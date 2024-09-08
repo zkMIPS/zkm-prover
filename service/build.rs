5583b4bc94d8d24f9f6a1cd8f0faefe5c6c59c22
xander42280 <xander.z@zkm.io>
2024-09-08 12:17:22 +0800

    Option proto3_optional

--- service/build.rs
+++ service/build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/src/proto/prover/v1/prover.proto")?;
-    tonic_build::compile_protos("proto/src/proto/stage/v1/stage.proto")?;
+    tonic_build::configure().protoc_arg("--experimental_allow_proto3_optional").compile(&["proto/src/proto/prover/v1/prover.proto"], &["proto/src/proto"])?;
+    tonic_build::configure().protoc_arg("--experimental_allow_proto3_optional").compile(&["proto/src/proto/stage/v1/stage.proto"], &["proto/src/proto"])?;
     Ok(())
 }
